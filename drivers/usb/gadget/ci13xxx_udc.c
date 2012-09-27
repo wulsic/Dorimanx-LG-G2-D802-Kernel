504938dfb4d55d1952f24c23fedc14789a1c08e4
Hemant Kumar <hemantk@codeaurora.org>
2012-09-27 16:42:36 -0700

    usb: gadget: Initialize non zero ep max packet size to 0xFFFF
    usb device controller driver initializes all endpoint's max packet
    size to 64 bytes. At the time of function driver bind gadget
    framework chooses endpoint to use by comparing it with endpoint
    descriptor of the function driver in ep_matches(). Driver bind
    fails for the function driver having isochronous endpoints, as
    ep max packet size is less than endpoint descriptor's max packet
    size. Hence set ep max packet size to 0xFFFF for non-zero
    endpoints in gadget driver registartion and endpoint disable.
    
    Change-Id: I9fc0a19bd4502812b8659222d60274ad8b9fef34
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>
    
    Conflicts:
    	drivers/usb/gadget/ci13xxx_udc.c

--- drivers/usb/gadget/ci13xxx_udc.c
+++ drivers/usb/gadget/ci13xxx_udc.c
@@ -2945,6 +2945,7 @@ static int ep_disable(struct usb_ep *ep)
 
 	mEp->desc = NULL;
 	mEp->ep.desc = NULL;
+	mEp->ep.maxpacket = USHRT_MAX;
 
 	spin_unlock_irqrestore(mEp->lock, flags);
 	return retval;
@@ -3474,7 +3475,7 @@ static int ci13xxx_start(struct usb_gadget_driver *driver,
 			mEp->ep.name      = mEp->name;
 			mEp->ep.ops       = &usb_ep_ops;
 			mEp->ep.maxpacket =
-				k ? (unsigned short)~0 : CTRL_PAYLOAD_MAX;
+				k ? USHRT_MAX : CTRL_PAYLOAD_MAX;
 
 			INIT_LIST_HEAD(&mEp->qh.queue);
 			spin_unlock_irqrestore(udc->lock, flags);
