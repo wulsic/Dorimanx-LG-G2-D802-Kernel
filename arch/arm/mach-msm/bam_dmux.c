1a459ab3191a47c1b7298944bbe3e6b3e6e4847f
alucard24 <dmbaoh2@gmail.com>
2014-08-28 15:45:06 +0200

    General corrections and implementations.

--- arch/arm/mach-msm/bam_dmux.c
+++ arch/arm/mach-msm/bam_dmux.c
@@ -2567,11 +2567,10 @@ struct device *bamDmux_pkt_dev;
 static ssize_t show_waketime(struct device *dev,
 		struct device_attribute *attr, char *buf)
 {
-	const size_t bufsz = sizeof(buf);
 	if (!bamDmux_pkt_dev)
 		return 0;
 
-	return snprintf(buf, bufsz, "%u\n", wakelock_timeout);
+	return snprintf(buf, sizeof(*buf), "%u\n", wakelock_timeout);
 }
 
 static ssize_t store_waketime(struct device *dev,
