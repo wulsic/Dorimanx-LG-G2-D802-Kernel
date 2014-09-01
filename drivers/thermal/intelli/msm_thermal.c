f035543c5589c305647f1901027eb4619081b09e
Alucard24 <dmbaoh2@gmail.com>
2014-09-01 15:53:16 +0200

    Restored default intelli_thermal parameter settings.

--- drivers/thermal/intelli/msm_thermal.c
+++ drivers/thermal/intelli/msm_thermal.c
@@ -46,12 +46,12 @@ static uint32_t hist_index = 0;
 static struct msm_thermal_data msm_thermal_info = {
 	.sensor_id = 0,
 	.poll_ms = DEFAULT_POLLING_MS,
-	.limit_temp_degC = 75,
-	.temp_hysteresis_degC = 5,
+	.limit_temp_degC = 70,
+	.temp_hysteresis_degC = 10,
 	.freq_step = 2,
 	.freq_control_mask = 0xf,
-	.core_limit_temp_degC = 75,
-	.core_temp_hysteresis_degC = 5,
+	.core_limit_temp_degC = 80,
+	.core_temp_hysteresis_degC = 10,
 	.core_control_mask = 0xe,
 };
 static uint32_t limited_max_freq_thermal = MSM_CPUFREQ_NO_LIMIT;
