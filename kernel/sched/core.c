4b1f695e14d5cd0feb773bd4469838a910e9ef51
alucard24 <dmbaoh2@gmail.com>
2014-09-20 15:23:00 +0200

    Revert "revert unthrottle_rt_rq"
    This reverts commit b3383a730fe9bf10dfe017464b2093b11da62526.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6496,6 +6496,15 @@ static void migrate_tasks(unsigned int dead_cpu)
 	 * value of rq->clock[_task]
 	 */
 	update_rq_clock(rq);
+	/* if there is one or more rt threads on the rq and if throttled,
+	 * we will deadlock in below loop. rt sched hrtimer have to run to
+	 * unthrottle the rt rq but irq is disabled in this context. Thus,
+	 * pick_next_task will not pick the rt task even if it is on the
+	 * runqueue. rq->nr_running never gets down to 1 and we will
+	 * loop forever here.
+	 * So we forcefully unthrottle the rt rq.
+	 */
+	unthrottle_rt_rq(rq);
 
 	for ( ; ; ) {
 		/*
