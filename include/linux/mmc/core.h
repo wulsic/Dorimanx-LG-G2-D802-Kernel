0860ac2dc853ee2639cbe35d3f0cc30487077c3d
alucard24 <dmbaoh2@gmail.com>
2014-09-30 21:52:09 +0200

    mmc: removed double mmc_flush_cache function declaration

--- include/linux/mmc/core.h
+++ include/linux/mmc/core.h
@@ -204,7 +204,6 @@ extern void mmc_set_ios(struct mmc_host *host);
 extern int mmc_detect_card_removed(struct mmc_host *host);
 extern int mmc_flush_cache(struct mmc_card *);
 
-extern int mmc_flush_cache(struct mmc_card *);
 extern int mmc_bkops_enable(struct mmc_host *host, u8 value);
 
 extern int mmc_detect_card_removed(struct mmc_host *host);
