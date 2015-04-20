b4911b1cc7bbb3fd2595a9c2205fcba88d62df61
Alucard24 <dmbaoh2@gmail.com>
2015-04-20 11:52:27 +0200

    removed useless cherryp.sh file.

--- cherryp.sh
+++
@@ -1,4 +0,0 @@
-git rev-list --reverse --topo-order 68321901053b634f27adb96c6323749e66d3db5d^..33c028ab962471762c7cc69d41f75e5d1a2ec58a | while read rev 
-do 
-  git cherry-pick $rev || break 
-done 
