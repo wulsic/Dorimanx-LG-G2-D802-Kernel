7b5d81d52a9b5d19746317de75bd177e09c87e4a
Alucard24 <dmbaoh2@gmail.com>
2015-04-28 12:36:40 +0200

    Removed unused slim cpu governor.

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -41,10 +41,6 @@
 struct device *stored_dev;
 #endif
 
-#ifdef CONFIG_CPU_FREQ_GOV_SLIM
-int graphics_boost = 4;
-#endif
-
 struct clk_pair {
 	const char *name;
 	uint map;
@@ -185,10 +181,6 @@ void kgsl_pwrctrl_pwrlevel_change(struct kgsl_device *device,
 	}
 
 	trace_kgsl_pwrlevel(device, pwr->active_pwrlevel, pwrlevel->gpu_freq);
-
-#ifdef CONFIG_CPU_FREQ_GOV_SLIM
-        graphics_boost = pwr->active_pwrlevel;
-#endif
 }
 
 EXPORT_SYMBOL(kgsl_pwrctrl_pwrlevel_change);
